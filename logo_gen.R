eae2a8ea53521d5272bb5d85851c06259ba86767
cigarplug <msainani@gmail.com>
2020-03-18 20:02:52 +1100

    logo export, no axes and labels

--- logo_gen.R
+++ logo_gen.R
@@ -25,4 +25,10 @@ ggplot() +
                 ) +
   geom_point(data = dots, aes(x = x, y = y), colour = "red", size = 8) +
   geom_line(data = border_up, aes(x = x, y = y), colour = "blue", size = 2)  +
-  geom_line(data = border_down, aes(x = x, y = y), colour = "blue", size = 2) 
+  geom_line(data = border_down, aes(x = x, y = y), colour = "blue", size = 2) +
+  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+        axis.text.y=element_blank(),axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),legend.position="none",
+        panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),plot.background=element_blank())
